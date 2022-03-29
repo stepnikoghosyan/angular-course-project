41e6c2b620e8cba44dee81244a17be5dfe76b12e
Stepan <snikoghosyan@servicetitan.com>
2022-03-29 19:28:21 +0400

    change url route to getFullRoute helper

--- src/app/shared/modules/layout/configs/navigation-items.config.ts
+++ src/app/shared/modules/layout/configs/navigation-items.config.ts
@@ -14,7 +14,7 @@ export function getNavigationItemsConfig(): INavigationItemsConfig[] {
     },
     {
       label: 'Users',
-      route: '/users', // TODO: update
+      route: getFullRoute(AppRoutes.Users),
       routerLinkActive: 'custom-active',
     },
     {
