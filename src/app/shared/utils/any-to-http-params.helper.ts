0b68d8f000c32580d1c4678edc406b97624f32cb
Stepan <snikoghosyan@servicetitan.com>
2022-03-29 19:07:41 +0400

    improve anyToHttpParams

--- src/app/shared/utils/any-to-http-params.helper.ts
+++ src/app/shared/utils/any-to-http-params.helper.ts
@@ -9,27 +9,38 @@ export function anyToHttpParams(obj?: IQueryParams): HttpParams {
   }
 
   for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      // Array is sent to API like this: propertyName1=value1&propertyName1=value2&propertyName1=value3
-      // If array is empty => remove it from query params
-      // If array item is null or undefined => remove it from array
-      if (obj[key] && Array.isArray(obj[key])) {
+    if (!obj.hasOwnProperty(key)) {
+      continue;
+    }
 
-        const arrayNotNullValues = (obj[key] as Array<any>).filter(val => val !== null && val !== undefined);
-        if (!arrayNotNullValues.length) {
-          return param;
-        }
-
-        param = param.set(key, obj[key][0]);
-
-        for (let i = 1; i < arrayNotNullValues.length; i++) {
-          param = param.append(key, arrayNotNullValues[i]);
-        }
-      } else if (obj[key]) {
-        param = param.set(key, obj[key] as string);
-      }
+    // Array is sent to API like this: propertyName1=value1&propertyName1=value2&propertyName1=value3
+    // If array is empty => remove it from query params
+    // If array item is null or undefined => remove it from array
+    if (obj[key] && Array.isArray(obj[key])) {
+      appendArrayToHttpParams(param, key, obj[key]);
+    } else if (obj[key]) {
+      param = param.set(key, obj[key] as string);
     }
   }
 
   return param;
+}
+
+function appendArrayToHttpParams(params: HttpParams, underKey: string, array?: any[]): HttpParams {
+  if (!array || !array.length) {
+    return params;
+  }
+
+  const arrayNotNullValues = array.filter(val => val !== null && val !== undefined);
+  if (!arrayNotNullValues.length) {
+    return params;
+  }
+
+  let newParams = params;
+
+  arrayNotNullValues.forEach((value) => {
+    newParams = newParams.append(underKey, value);
+  });
+
+  return newParams;
 }
